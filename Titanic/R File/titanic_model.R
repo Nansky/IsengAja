bd93359306da73f8645fc25c98a4f0d467824caa
Nansky <nanda.arta89@gmail.com>
2018-07-19 18:32:50 +0700

    Commit Update Titanic Model

--- Titanic/R File/titanic_model.R
+++ Titanic/R File/titanic_model.R
@@ -6,7 +6,9 @@ library(dplyr)
 library(Amelia)
 library(ggplot2)
 
-train <- read.csv('titanic_train.csv')
+setwd("/Users/nanskyarts/Documents/NaN/Personal/Data Science (DS)/R/Projects/Online/Kaggle/Titanic/")
+
+train <- read.csv('Dataset/titanic_train.csv')
 
 missing <- missmap(obj = train, main = "Missing Value", legend = F, col = c('yellow', 'black'), y.at=1)
 
@@ -32,6 +34,28 @@ impute_age <- function(age,class){
     return(out)
 }
 
+impute_test_age <- function(age,class){
+    out <- age
+    for (i in 1:length(age)){
+        if(is.na(age[i])){
+            if(class[i] == 1){
+                out[i] <- 43
+            }
+            else if(class[i] == 2){
+                out[i] <- 26
+            }
+            else{
+                out[i] <- 24
+            }
+        }
+        else{
+            out[i] <- age[i]
+        }
+    }
+    
+    return(out)
+}
+
 ## Feature Selection ##
 new_age_train <- impute_age(train$Age, train$Pclass)
 train$Age <- new_age_train
@@ -44,12 +68,12 @@ logistic_regression <- glm(Survived ~ .,family = binomial(link = 'logit'), data=
 new_logit_model <- step(logistic_regression)
 
 ## Read Test_data
-test <- read.csv('titanic_test.csv')
+test <- read.csv('Dataset/titanic_test.csv')
 any(is.na(test)) #TRUE
 
 missing <- missmap(obj = test, main = "Missing Value", legend = F, col = c('yellow', 'black'), y.at=1)
 
-new_age_test <- impute_age(test$Age, test$Pclass)
+new_age_test <- impute_test_age(test$Age, test$Pclass)
 test$Age <- new_age_test
 
 #missing <- missmap(obj = test, main = "Missing Value", legend = F, col = c('yellow', 'black'), y.at=1)
@@ -60,7 +84,7 @@ fitted.res <- ifelse(predicted > 0.5,1,0)
 names(predicted) <- "Survived"
 
 # read Gender Submission Data 
-gender_sub <- read.csv('gender_submission.csv')
+gender_sub <- read.csv('Dataset/gender_submission.csv')
 
 # Count MissclassError
 missclassEr <- mean(gender_sub$Survived != fitted.res)
@@ -71,4 +95,5 @@ submission_data <- cbind(gender_sub$PassengerId, submission_data)
 colnames(submission_data) <- c("PassengerID", "Survived")
 
 
-write.csv(submission_data, file = "My Submission.csv")
+write.csv(submission_data, file = "Output/My Submission.csv", row.names = FALSE)
+
